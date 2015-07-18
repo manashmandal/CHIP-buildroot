b6725f2c254a7468e54ac350af3f64943bfd7cae
Alex Kaplan <kaplan2539@gmail.com>
2015-07-18 00:47:14 +0000

    added CHIP logo

--- board/nextthing/chip/add_hwtest.sh
+++ board/nextthing/chip/add_hwtest.sh
@@ -11,3 +11,31 @@ install -m 0755 ${SCRIPT_DIR}/hwtest.sh ${TARGET_DIR}/usr/bin/hwtest.sh
 install -m 0755 ${SCRIPT_DIR}/battery.sh ${TARGET_DIR}/usr/bin/battery.sh
 install -m 0755 ${SCRIPT_DIR}/power.sh ${TARGET_DIR}/usr/bin/power.sh
 
+echo -e "\\n
+          MM      MMM      MM.          \n\
+          MM      MMM     .MM           \n\
+          MM      MMM      MM.          \n\
+     MMMMMMMMMMMMMMMMMMMMMMMMMMMMM      \n\
+    .MM                         MM.     \n\
+    .MM                        .MM.     \n\
+MMMMMMM                        .MMMMMMM \n\
+MMMMMMM      MMMM              .MMMMMMM \n\
+    .MM     MMMMMM.            .MM.     \n\
+    .MM    8MMMMMM,            .MM.     \n\
+    .MM     MMMMMMMM.          .MM.     \n\
+MMMMMMM     .....MMMMMMMMMMMMMMMMMMMMMN \n\
+MMMMMMM             .:MMMMMMMMMMMMMMMMM.\n\
+    .MM                        .MM.     \n\
+    .MM                        .MM.     \n\
+    .MM                        .MM.     \n\
+8MMMMMM       Next Thing Co    .MMMMMMO \n\
+MMMMMMM                        .MMMMMMM:\n\
+.    MM                        .MM      \n\
+    .MM                        .MM.     \n\
+    .MMMMMMMMMMMMMMMMMMMMMMMMMMMMM      \n\
+          MM      MMM     .MM           \n\
+          MM      MMM     .MM           \n\
+          MM      MMM     .MM           \n\
+          .Z       M      .O            \n\
+\n\
+" > ${TARGET_DIR}=/etc/issue
